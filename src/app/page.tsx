e02105ff4d38ef26da2ccefe488d36eb54e0d895
Denis <denmih61@gmail.com>
2023-06-09 12:24:18 +0300

    new app

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.tsx</code>
+        </p>
+    </main>
+  )
+}
